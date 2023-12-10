1737b279cf59b9b08240cd369a79f72b98f8954f
Jasmin Chen <oo13847@gmail.com>
2023-12-10 23:53:04 +0800

    專案初始化，建立

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import naive from 'naive-ui';
+import router from './router';
+import './styles/styles.scss';
+const app = createApp(App);
+
+app.use(router);
+app.use(naive);
+
+app.mount('#app');
---
+++ src/views/LoginView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="logoWrap">
+    <img src="/img/logo.jpg" alt="" />
+    <div class="loginWrap">
+      <div class="loginImg">
+        <img src="/img/logoIcon.png" alt="" />
+      </div>
+      <n-space vertical>
+        <div class="loginInputWrap">
+          <n-input
+            v-model:value="username"
+            placeholder="Username"
+            id="username"
+            @keydown.enter="onSubmit()"
+            :maxlength="20"
+            class="loginInputStyle"
+            size="large"
+          >
+            <template #prefix>
+              <n-icon :component="People" />
+            </template>
+          </n-input>
+        </div>
+        <div class="loginInputWrap">
+          <n-input
+            v-model:value="password"
+            type="password"
+            show-password-on="click"
+            placeholder="Password"
+            :maxlength="20"
+            size="large"
+            class="loginInputStyle"
+            id="password"
+            @keydown.enter="onSubmit()"
+          >
+            <template #prefix>
+              <n-icon :component="LockOpen" />
+            </template>
+          </n-input>
+        </div>
+      </n-space>
+      <div class="loginBtnWrap">
+        <button>登入</button>
+        <div class="visitorWrap">
+          <div>
+            <span><div></div></span>
+            <p>或</p>
+            <span><div></div></span>
+          </div>
+          <p>使用訪客登入</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { People, LockOpen } from '@vicons/ionicons5';
+import { ref } from 'vue';
+const username = ref('');
+const password = ref('');
+
+const onSubmit = () => {};
+</script>
