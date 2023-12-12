4cc48dab954530d1eddab47c9ca248210b830af2
Jasmin Chen <oo13847@gmail.com>
2023-12-12 22:40:42 +0800

    npm install pinia

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import App from './App.vue';
 import naive from 'naive-ui';
 import router from './router';
 import './styles/styles.scss';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia()
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 app.use(naive);
 
