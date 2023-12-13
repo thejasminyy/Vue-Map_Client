f3fb8767e941ef2b78656c7b3aad120615642a76
Jasmin <oo138@DESKTOP-4589MB5>
2023-12-13 08:50:48 +0800

    修改@路徑問題 把URL指到./src 就可以使用@

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import { RouterView } from 'vue-router'
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -6,7 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }, 
   },
 });
