ed3d6d69985dbba07822d6d03878535fea41e371
Jasmin Chen <oo13847@gmail.com>
2023-12-14 23:55:19 +0800

    新增 header 按鈕

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import Header from "@/views/Header.vue";
 </script>
 
 <template>
+  <Header />
   <router-view></router-view>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,4 @@
 <template>
-
+  <div class="home"></div>
 </template>
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
