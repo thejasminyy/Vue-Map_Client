0d0d9041f2ab2f5e650efc7beb91c38773308f8c
Jasmin Chen <oo13847@gmail.com>
2023-12-19 23:40:39 +0800

    新增 關於我 & 地圖的路徑 & vue檔

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,23 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <n-carousel autoplay>
+      <img
+        class="carousel-img"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
+      />
+      <img
+        class="carousel-img"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
+      />
+      <img
+        class="carousel-img"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
+      />
+      <img
+        class="carousel-img"
+        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
+      />
+    </n-carousel>
+  </div>
 </template>
 <script setup lang="ts"></script>
