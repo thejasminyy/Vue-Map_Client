f74f62dc9a772b529f75a0900c8a585815c4dbdd
Jasmin Chen <oo13847@gmail.com>
2023-12-26 23:24:45 +0800

    新增技術 經歷

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,26 +11,36 @@
     </div>
     <div class="mainWrap">
       <div class="item">
-        <p>關於我</p>
-        <div class="detailed">
-          <p>
-            我是一名軟體工程師，擁有三年的網頁前端開發經驗，主要接觸項目為：網頁3D模型互動功能、網頁前端排版、IoT設備串接、CCTV影像串接。
-            工作中主要使用TypeScript、Vue3.js、Scss做為開發工具。
-          </p>
-          <p>
-            除了完成本身的開發工作，過去一年間逐步勝任3-5人的開發團隊領導職務，期間完成了3個大型專案，累積了很多的問題處理方式，也訂定專案的標準作業流程。
-            曾多次代表公司與客戶進行業務交流和合作溝通，了解客戶需求並規劃功能架構與團隊討論。這些經驗使我不僅具備了開發技能，還培養了良好的領導能力和團隊合作能力。
-          </p>
-        </div>
+        <p><n-icon :component="AccountCircleSharp" /> 關於我</p>
+        <ul class="detailed">
+          <li>軟體工程師</li>
+          <li>三年網頁前端開發</li>
+          <li>過去一年勝任5人的開發團隊領導職務</li>
+        </ul>
       </div>
       <div class="item">
-        <p>技能</p>
+        <p><n-icon :component="CalligraphyPen24Regular" />技能</p>
+        <ul class="detailed">
+          <li>
+            Front-end： HTML5 / CSS3 / Scss / RWD響應式網頁設計 / jQuery /
+            JavaScript / TypeScript / Vue3.js / React
+          </li>
+          <li>Communication： HTTP / MQTT</li>
+          <li>Database： MS SQL / MySQL</li>
+          <li>Other： IIS / nginx / Git / SVN</li>
+        </ul>
       </div>
       <div class="item">
-        <p>經歷</p>
+        <p><n-icon :component="BookToolbox20Regular" />經歷</p>
+        <ul class="detailed">
+          <li>研發工程師 - 天茶智能科技 Sep 2020 - Feb 2024</li>
+        </ul>
       </div>
     </div>
     <div class="decorate"></div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { AccountCircleSharp } from "@vicons/material";
+import { CalligraphyPen24Regular, BookToolbox20Regular } from "@vicons/fluent";
+</script>
