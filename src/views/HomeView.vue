808e26870d02bf7189daa7f08bf29daab3d866d8
Jasmin Chen <oo13847@gmail.com>
2024-01-21 23:50:13 +0800

    1.新增切換事件 2.判斷icon 3.寫假資料 接資料

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,55 +14,36 @@
               </n-icon>
             </template>
           </n-timeline-item>
-          <n-timeline-item
-            type="success"
-            title="登山"
-            content="玉山主峰"
-            time="2023-01-03 20:46"
-          />
-          <n-timeline-item
-            type="success"
-            title="登山"
-            content="奇萊主峰、北峰"
-            time="2023-03-27 13:50"
-          />
-          <n-timeline-item
-            type="success"
-            title="東京九日遊"
-            content="富士山"
-            time="2023-04-01"
-          />
-          <n-timeline-item
-            type="success"
-            title="志佳陽大山"
-            content="單攻"
-            time="2023-05-25"
-          />
-          <n-timeline-item
-            type="success"
-            title="小關山"
-            content="神池與小關山"
-            time="2023-08-25"
-          />
-          <n-timeline-item
-            type="warning"
-            title="J的生日"
-            content="必須準備驚喜!!"
-            time="2023-09-21"
-          />
-          <n-timeline-item
-            type="warning"
-            title="資深同事提出離職"
-            content="太令人傷心的事情!"
-            time="2023-11-15"
-          />
-          <n-timeline-item
-            type="info"
-            title="J提出離職"
-            content="提出離職"
-            time="2023-11-15"
-            line-type="dashed"
-          />
+          <template v-for="(item, index) in data" :key="index">
+            <n-timeline-item
+              class="active"
+              type="success"
+              :title="item.title"
+              :content="item.content"
+              :time="item.time"
+              @click="changeItem(index)"
+              v-if="index === nowItme"
+            >
+              <template #icon>
+                <n-icon>
+                  <RadioButtonCheckedFilled />
+                </n-icon>
+              </template>
+            </n-timeline-item>
+            <n-timeline-item
+              :title="item.title"
+              :content="item.content"
+              :time="item.time"
+              @click="changeItem(index)"
+              v-else
+            >
+              <template #icon>
+                <n-icon>
+                  <RadioButtonUncheckedFilled />
+                </n-icon>
+              </template>
+            </n-timeline-item>
+          </template>
           <n-timeline-item content="2023">
             <template #icon>
               <n-icon>
@@ -85,8 +66,68 @@
 <script setup lang="ts">
 // import { storeToRefs } from "pinia";
 // import { ImageEdit16Regular } from "@vicons/fluent";
-import { CalendarTodayRound } from "@vicons/material";
+import {
+  CalendarTodayRound,
+  RadioButtonUncheckedFilled,
+  RadioButtonCheckedFilled,
+} from "@vicons/material";
+
+import { ref } from "vue";
+
 // import { useUserStore } from "@/stores/user";
 // const userPinia = useUserStore();
 // const { loginStatus } = storeToRefs(userPinia);
+const nowItme = ref(0);
+
+/**
+ * 切換item
+ * @param num index
+ */
+const changeItem = (num: number) => {
+  if (nowItme.value === num) return;
+  nowItme.value = num;
+};
+
+const data = ref([
+  {
+    title: "登山",
+    content: "玉山主峰",
+    time: "2023-01-03 20:46",
+  },
+  {
+    title: "登山",
+    content: "奇萊主峰、北峰",
+    time: "2023-03-27 13:50",
+  },
+  {
+    title: "東京九日遊",
+    content: "富士山",
+    time: "2023-04-01",
+  },
+  {
+    title: "志佳陽大山",
+    content: "單攻",
+    time: "2023-05-25",
+  },
+  {
+    title: "小關山",
+    content: "神池與小關山",
+    time: "2023-08-25",
+  },
+  {
+    title: "J的生日",
+    content: "必須準備驚喜",
+    time: "2023-09-21",
+  },
+  {
+    title: "資深同事提出離職",
+    content: "太令人傷心的事情",
+    time: "2023-11-15",
+  },
+  {
+    title: "J提出離職",
+    content: "提出離職",
+    time: "2023-11-15",
+  },
+]);
 </script>
