657906583ca6c903ca16b99dd5b7f72aa0a10ecb
Jasmin Chen <oo13847@gmail.com>
2024-04-29 22:26:31 +0800

    間隔修改

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -2,29 +2,29 @@ import { defineStore } from 'pinia'
 
 
 export const useUserStore = defineStore('user', {
-    state() {
-        return {
-          /** 登入狀態 false未登入 true訪客 */
-            loginStatus:false as boolean,
-            userName:''as string,
-            loginTime:''as string,
-        };
+  state() {
+      return {
+        /** 登入狀態 false未登入 true訪客 */
+          loginStatus:false as boolean,
+          userName:''as string,
+          loginTime:''as string,
+      };
+    },
+  actions: {
+    /** 初始化登入資訊 */
+    initLoginData(){
+      this.loginStatus = false;
+      this.userName = "";
+      this.loginTime = "";
+      sessionStorage.clear();
+    }
+  },
+  persist: {
+    // 只存取 loginStatus
+    strategies: [
+      {
+        paths: ['loginStatus','userName','loginTime'],
       },
-    actions: {
-      /** 初始化登入資訊 */
-      initLoginData(){
-        this.loginStatus = false;
-        this.userName = "";
-        this.loginTime = "";
-        sessionStorage.clear();
-      }
-    },
-    persist: {
-      // 只存取 loginStatus
-      strategies: [
-        {
-          paths: ['loginStatus','userName','loginTime'],
-        },
-      ],
-    },
-  })
+    ],
+  },
+})
